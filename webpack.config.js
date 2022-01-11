6b9780f50097c512d5b5d48b798251ba6dbb0145
noumecha <noumechaivan@gmail.com>
2022-01-11 08:36:19 +0100

    modifs: TMC

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ const devMode = process.env.NODE_ENV !== "production";
 
 const plugins = [];
 const htmlDatas = [""];
-const CurrentThemeName = "gp";
+const CurrentThemeName = "vesperr";
 plugins.push(
   new MiniCssExtractPlugin({
     filename: "css/[name].css",
