05b8df9cb37ef34deeaba66f226da338db7b1e3e
stephane888 <kksasteph888@gmail.com>
2022-02-15 13:46:27 +0100

    MAJ

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ const devMode = process.env.NODE_ENV !== "production";
 const plugins = [];
 
 const htmlDatas = [""];
-const CurrentThemeName = "mckinsey";
+const CurrentThemeName = "rc-web";
 
 //const htmlDatas = [];
 const htmlDatasKey = [];
