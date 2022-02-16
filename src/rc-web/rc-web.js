8ea3ec481a8d6e9b4de86883fbecbeafd9cf8f3c
stephane888 <kksasteph888@gmail.com>
2022-02-16 04:18:18 +0100

    MAJ3

--- src/rc-web/rc-web.js
+++ src/rc-web/rc-web.js
@@ -6,9 +6,8 @@ import "../gp/lib/aos/aos.css";
 
 import "@stephane888/wbu-atomique/js/bootstrap/all.js";
 
-// import "./headers/header.js";
+import "./HeaderSection/header.js";
 import "./HeroSection/hero.js";
-import "./HeaderSection/header";
 import "./contactSection/contact";
 import "./list-section/list-section";
 import "./priceBlock/price";
