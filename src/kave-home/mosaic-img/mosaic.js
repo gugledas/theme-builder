7156b5fa1ab9d0de7eea48d668c4080914f809cc
gugledas <azabzistany@gmail.com>
2022-03-11 11:29:19 +0100

    Maj

--- src/kave-home/mosaic-img/mosaic.js
+++ src/kave-home/mosaic-img/mosaic.js
@@ -2,17 +2,18 @@ import "lightgallery/css/lightgallery-bundle.min.css";
 import "lightgallery/css/lg-zoom.css";
 // import "lightgallery/fonts/lg.ttf";
 import lightGallery from "lightgallery";
-import lgZoom from "lightgallery/plugins/zoom";
+//import lgZoom from "lightgallery/plugins/zoom";
 
 import "./mosaic.html";
 import "./mosaic.scss";
 
 lightGallery(document.getElementById("my-mosaic"), {
-  plugins: [lgZoom],
+  //plugins: [lgZoom],
   selector: ".gallery-item",
   hideControlOnEnd: true,
   backdropDuration: 100,
   mode: "fade",
+  speed: 500,
   download: false
   // ... other settings
 });
