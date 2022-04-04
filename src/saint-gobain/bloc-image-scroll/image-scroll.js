818174d5126edc1f77f1e009530bb409070d3933
gugledas <azabzistany@gmail.com>
2022-04-04 18:18:44 +0100

    image-scroll

---
+++ src/saint-gobain/bloc-image-scroll/image-scroll.js
@@ -0,0 +1,24 @@
+import "./image-scroll.html";
+import "./image-scroll.scss";
+
+function setImage(img) {
+  var image = img.src;
+  var dataset = img.dataset.src;
+  img.addEventListener(
+    "mouseover",
+    () => {
+      img.src = dataset;
+      //removeEvent(img);
+    },
+    true
+  );
+  img.removeEventListener("mouseover", () => {}, false);
+  //;
+}
+
+var img = document.querySelectorAll(".the-image");
+
+for (let im of img) {
+  console.log("img", im);
+  setImage(im);
+}
