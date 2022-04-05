9af1405aa3132c116a4aaf8a4070e76596e6aa37
nzouda777 <rodriguenzouda@gmail.com>
2022-04-05 15:26:35 +0100

    MAJ

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ const devMode = process.env.NODE_ENV !== "production";
 
 const plugins = [];
 
-const CurrentThemeName = "offrord-team";
+const CurrentThemeName = "new-offroad";
 
 plugins.push(
   new MiniCssExtractPlugin({
