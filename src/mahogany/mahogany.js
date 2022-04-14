9b6fc1c2ca8932204f51af3e54bad363f118b730
noumecha <noumechaivan@gmail.com>
2022-04-14 17:51:01 +0100

    maj : tmc -- spaker

---
+++ src/mahogany/mahogany.js
@@ -0,0 +1,8 @@
+// === importation des librairies & utils === //
+import "@stephane888/wbu-atomique/js/bootstrap/all.js";
+import "@stephane888/wbu-atomique/scss/bootstrap-all.scss";
+import "@stephane888/wbu-atomique/scss/atome/typography/_default.scss";
+import "./assets/js/beacon.min.js";
+
+// importation des section :
+import "./header/header";
