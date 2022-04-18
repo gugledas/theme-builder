a22c501f7b5119ec9e8be286d27d9092b628375d
noumecha <noumechaivan@gmail.com>
2022-04-18 10:00:38 +0100

    maj : by tmc

--- src/mahogany/header/header.js
+++ src/mahogany/header/header.js
@@ -1,30 +1,4 @@
 import "./header.html";
 import "./header.scss";
-import "../assets/js/jquery-3.6.0.min";
-//$(document).ready(function(){});
-jQuery(function() { });
 
 //alert("js ok");
-
-/**
- * mobile nav-bar activation with js 
- */
-on("click",".mobile-nav-toggle", function(e){
-    alert("this event listener is ok dukes");
-    select("#navbar").classList.toggle("navbar-mobile");
-    this.classList.toogle("bi-list");
-    this.classList.toggle("bi-x");
-});
-
-/**
- * mobile nav dropdowns activation
- */
-on("click",
-   ".navbar .dropdown > a",
-   function (e) {
-       if(select("#navbar").classList.contains("navbar-mobile")){
-           e.preventDefault();
-           this.nextElementSibling.classList.toggle("dropdown-active")
-       }
-   }
-);
--- src/mahogany/mahogany.js
+++ src/mahogany/mahogany.js
@@ -2,7 +2,10 @@
 import "@stephane888/wbu-atomique/js/bootstrap/all.js";
 import "@stephane888/wbu-atomique/scss/bootstrap-all.scss";
 import "@stephane888/wbu-atomique/scss/atome/typography/_default.scss";
-import "./assets/js/beacon.min.js";
+import "./lib/js/beacon.min.js";
+import "./lib/js/jquery-3.6.0.min";
 
-// importation des section :
+// importation des sections :
 import "./header/header";
+//import "./hero-section/hero";
+import "./consult-section/consult";
