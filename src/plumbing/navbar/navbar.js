b7a801dba79570062d7d5e91c18c1e1f4bed46be
gugledas <azabzistany@gmail.com>
2022-04-23 14:45:12 +0100

    ajout du menu plumbing

--- src/plumbing/navbar/navbar.js
+++ src/plumbing/navbar/navbar.js
@@ -1,98 +1,202 @@
 import "./navbar.html";
 import "./navbar.scss";
 
+let select = (element) => document.querySelector(element);
+let header = select(".plumbing-navbar");
+var ul = header.querySelectorAll("nav .nav-list > li");
+const toggleNav = () => {
+  var count = 0;
+
+  select(".bar").addEventListener("click", () => {
+    header.classList.toggle("active");
+    var ul = select(".nav-list");
+
+    ul.classList.add("nav-list-mobile");
+
+    ul.classList.remove("nav-list");
+    while (count < 1) {
+      setToggleUl();
+      count++;
+    }
+  });
+  select(".hide").addEventListener("click", () => {
+    header.classList.toggle("active");
+    var ul = select(".nav-list-mobile");
+    ul.classList.remove("nav-list-mobile");
+    ul.classList.add("nav-list");
+    //setToggleUl();
+  });
+
+  /* breakpoint */
+  let small = window.matchMedia("(max-width :768px)");
+  small.addEventListener("change", hideMenuSM);
+
+  //
+};
+
+toggleNav();
+
+/* cacher les nav lorsque besoin s'impose */
+
+/* breakpoint */
+let first = window.matchMedia("(max-width :1200px)");
+
+first.addEventListener("change", copyNode);
+let second = window.matchMedia("(max-width :992px)");
+second.addEventListener("change", copyNode2);
+
+let subuild = header.querySelector(".sub-build");
+let navLi = Array.from(header.querySelectorAll("nav .nav-list > li"));
+
+if (navLi && navLi.length) {
+}
+/* permet de reduire les nav en dessous des breakpoint egale 1200px */
+function copyNode2(bp) {
+  let child = Array.from(document.querySelectorAll(".sub-build > ul > li "));
+  let navLi2 = Array.from(header.querySelectorAll("nav .nav-list > li"));
+  if (bp.matches) {
+    subuild.style.display = "flex";
+    if (navLi.length) {
+      for (let i = navLi2.length - 3; i > 1; i--) {
+        (function (rs) {
+          subuild.querySelector("ul").appendChild(rs);
+        })(navLi2[i]);
+      }
+    }
+  } else {
+    for (let i = child.length - 1; i > child.length - 3; i--) {
+      (function (rs) {
+        if (rs) {
+          subuild.parentElement.insertBefore(rs, subuild);
+        }
+      })(child[i]);
+    }
+  }
+}
+/* permet de reduire les nav en dessous des breakpoint egale 1200px */
+function copyNode(bp) {
+  let child = document.querySelectorAll(".sub-build > ul > li ");
+  let navLi = Array.from(header.querySelectorAll("nav .nav-list > li"));
+  if (bp.matches) {
+    subuild.style.display = "flex";
+    if (navLi.length) {
+      for (let i = navLi.length - 3; i > 3; i--) {
+        (function (rs) {
+          subuild.querySelector("ul").appendChild(rs);
+        })(navLi[i]);
+      }
+    }
+  } else {
+    for (let ch of child) {
+      (function (rs) {
+        subuild.parentElement.insertBefore(rs, subuild);
+      })(ch);
+    }
+    subuild.style.display = "";
+  }
+}
+function rebuildNav() {
+  let windowWidth = window.innerWidth;
+  if (windowWidth > 992 && windowWidth < 1201) {
+    copyNode({ matches: true });
+  }
+  if (windowWidth < 992 && windowWidth >= 768) {
+    console.log("> 768", windowWidth);
+    copyNode2({ matches: true });
+  }
+  if (windowWidth <= 768) {
+    console.log("< 768", windowWidth);
+    copyNode({ matches: false });
+  }
+}
+/* gestion de la navigation après le chargement de la page */
+window.addEventListener("DOMContentLoaded", (event) => {
+  rebuildNav();
+});
+
+/*Gère l'affichage du menu sur petit écran*/
+function hideMenuSM(bp) {
+  if (!bp.matches) {
+    select(".plumbing-navbar").classList.remove("active");
+    select(".plumbing-navbar nav > ul").classList.remove("nav-list-mobile");
+    select(".plumbing-navbar nav > ul").classList.add("nav-list");
+    rebuildNav();
+  } else {
+    rebuildNav();
+  }
+  //setToggleUl();
+}
+
+/* permet de gérer les menu deroulant sur petit écran au click du burger*/
+function setToggleUl() {
+  var subMenu = header.querySelectorAll(".nav-list-mobile .sub-alt > a");
+  if (subMenu) {
+    for (let sub of subMenu) {
+      sub.addEventListener("click", () => {
+        var all = sub.parentElement.parentElement.querySelectorAll(
+          sub.parentElement.parentElement.tagName + " > .sub-alt--open > a"
+        );
+
+        if (all && all.length) {
+          for (let li of all) {
+            (function (gr) {
+              if (gr === sub) {
+                //console.log("same", li);
+              } else {
+                trueHeight(gr);
+                gr.parentElement.classList.toggle("sub-alt--open");
+
+                let list =
+                  gr.parentElement.querySelectorAll(".sub-alt--open > a");
+                // console.log("sub", gr);
+                if (list && list.length) {
+                  for (let sous of list) {
+                    trueHeight(sous);
+                    sous.parentElement.classList.toggle("sub-alt--open");
+                  }
+                }
+              }
+            })(li);
+          }
+        }
+        trueHeight(sub);
+        sub.parentElement.classList.toggle("sub-alt--open");
+      });
+    }
+  }
+}
+/* gere le max-height des ul au click */
+function trueHeight(a) {
+  var parent = header.querySelectorAll(".nav-list-mobile");
+  var ulNext = a.nextElementSibling;
+  if (ulNext && parent) {
+    if (ulNext) {
+      if (ulNext.style.maxHeight) {
+        ulNext.style.maxHeight = null;
+      } else {
+        ulNext.style.maxHeight = ulNext.scrollHeight + "px";
+      }
+    }
+  } else {
+    console.error("ulnext n'existe pas");
+  }
+}
+
+/* fixe le menu au dessus */
 const onscroll = (el, listener) => {
   el.addEventListener("scroll", listener);
 };
-var nav = document.querySelector(".navbar-section-plumbing");
+var nav = document.querySelector(".plumbing-navbar");
 
 if (nav) {
   let header = nav.offsetTop;
-  var next = nav.nextElementSibling;
   const fixedTop = () => {
-    if (header - window.scrollY <= 0) {
-      // console.log("arr", header, window.scrollY);
-      nav.classList.add("pl-fixed-top");
+    if (window.pageYOffset > header) {
+      nav.classList.add("plumbing-navbar--fixed");
     } else {
-      nav.classList.remove("pl-fixed-top");
+      nav.classList.remove("plumbing-navbar--fixed");
     }
   };
   window.addEventListener("load", fixedTop);
   onscroll(document, fixedTop);
 }
-
-let mobile = document.querySelector(".nsp-mobile-menu");
-let burger = document.querySelector(".nsp-nav-toggler");
-let close = document.querySelector(".menu-hide");
-if (mobile && burger && close) {
-  const toggler = () => {
-    mobile.classList.toggle("mobile-menu-active");
-  };
-  burger.addEventListener("click", toggler);
-  close.addEventListener("click", toggler);
-}
-
-let ul1 = document.querySelectorAll(".mobile-menu .nsp-nav-ul .nsp-nav-link");
-
-let hideTogleBtn = (el) => {
-  return new Promise((resolv) => {
-    let a = el.querySelector("a");
-    let ul = el.querySelector("ul");
-
-    if (ul) {
-      let liParent = el.parentElement.classList.contains("sub-menu");
-      if (liParent) {
-        a.querySelector("button").classList.remove("lit-btn--bg");
-      }
-      //ul.classList.add("d-none");
-      a.addEventListener(
-        "click",
-        (e) => {
-          var all = el.parentElement.querySelectorAll("li.nav-ul-opened");
-          console.log("elle", e.target.parentElement.tagName);
-          if (all && all.length) {
-            for (let li of all) {
-              if (li === el) {
-                console.log("same", el);
-              } else {
-                (function (gr) {
-                  let a = gr.querySelector("a");
-                  let ul = gr.querySelector("ul");
-
-                  gr.classList.toggle("nav-ul-opened");
-                  a.querySelector("button").classList.toggle("lit-btn-opened");
-                })(li);
-              }
-            }
-          }
-
-          el.classList.toggle("nav-ul-opened");
-          a.querySelector("button").classList.toggle("lit-btn-opened");
-          //el.classList.toggle("nav-ul-opened");
-        },
-        true
-      );
-    } else {
-      a.querySelector("button").classList.add("d-none");
-    }
-    resolv(el);
-  });
-};
-//console.log("nav_mb:", nav_mb);
-let listElement = (parent) => {
-  if (parent && parent.length) {
-    for (let el of parent) {
-      hideTogleBtn(el).then((re) => {
-        console.log("ok", re);
-      });
-    }
-  }
-};
-listElement(ul1);
-//je veux selectionner tous les li
-/* 
-even doit toujour rechercher tous les li.activé et les désactivés
-en suite selectionner l'element sur lequel on a cliqué
- */
-
-//je veux que
--- src/plumbing/plumbing.js
+++ src/plumbing/plumbing.js
@@ -5,11 +5,11 @@ import "@stephane888/wbu-atomique/scss/bootstrap-all.scss";
 import "@stephane888/wbu-atomique/js/bootstrap/all.js";
 import "@stephane888/wbu-atomique/scss/atome/typography/_default.scss";
 
-import "./Hero/hero.js";
 import "./navbar/navbar.js";
-import "./help-section/help";
-import "./service-section/service";
-import "./team-section/team";
-import "./bg-fixed/bg-fixed";
-import "./svg-service/svg-service";
-import "./why-section/why-us";
+import "./Hero/hero.js";
+// import "./help-section/help";
+// import "./service-section/service";
+// import "./team-section/team";
+// import "./bg-fixed/bg-fixed";
+// import "./svg-service/svg-service";
+// import "./why-section/why-us";
