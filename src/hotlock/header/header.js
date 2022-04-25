8460ec5601d05394b23c1eda5667c9a45c91c2dc
nzouda777 <rodriguenzouda@gmail.com>
2022-04-25 17:31:21 +0100

    MAJ

--- src/hotlock/header/header.js
+++ src/hotlock/header/header.js
@@ -14,3 +14,31 @@ var swiper = new Swiper(".mySwiper", {
       clickable: true,
     },
   });
+
+
+let bacTop = document.querySelector(".bac-top-btn");
+bacTop.addEventListener("click", scrollUp);
+
+window.onscroll = () => {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 500 ||
+    document.documentElement.scrollTop > 500
+  ) {
+    bacTop.style.visibility = "visible";
+    bacTop.style.opacity = "1";
+  } else {
+    bacTop.style = "";
+  }
+}
+
+function scrollUp() {
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: "smooth"
+  });
+}
