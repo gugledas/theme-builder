5472f5daee6aeafe05b873a25fbda53388130b43
noumecha <noumechaivan@gmail.com>
2022-04-26 15:11:47 +0100

    maj : by tmc --end js help section

--- src/mahogany/help-section/help.js
+++ src/mahogany/help-section/help.js
@@ -2,13 +2,43 @@ import "./help.html";
 import "./help.scss";
 
 // js by tmc with event listener for the theme 
+//function test() {
+//    alert("welcome in this theme");
+//}
 
-let dis = document.getElementById("btn-two");
+function showMe() {
+    document.querySelectorAll(".btn-full").forEach(button =>{
+        button.addEventListener("click", () => {
+            const sideBar = button.parentElement;
+            const tabsContainer = document.getElementById("content-container");
+            const tabNumber = button.dataset.forTab;
+            let num = tabNumber.toString();
+            const tabToActivate = tabsContainer.querySelector('.content[data-tab='+'"'+ num +'"]');
 
-dis.addEventListener("click",displayMe);
+            sideBar.querySelectorAll('.btn-full').forEach(button => {
+                let t = document.getElementsByClassName('btn-full--active');
+                for (let index = 0; index < t.length; index++) {
+                    t[index].classList.remove('btn-full--active');
+                }
+                button.classList.remove('btn-full--active');
+            });
 
-function displayMe() {
-    bloc = document.getElementById("content-two");
-    bloc.style.display = "block";
+            tabsContainer.querySelectorAll('.content').forEach(contener => {
+                contener.classList.remove('content--active');
+            });
+
+            button.classList.add('btn-full--active');
+            tabToActivate.classList.add('content--active');
+
+            console.log(sideBar);
+            console.log(tabsContainer);
+            console.log(tabNumber);
+            console.log(tabToActivate);
+
+        });
+    });
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+    showMe();
+});
