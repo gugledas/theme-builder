a9a75ca1db5aa04a47437a5866f232b167563a0f
noumecha <noumechaivan@gmail.com>
2022-04-29 09:54:40 +0100

    maj : by tmc

--- src/mahogany/header/header.js
+++ src/mahogany/header/header.js
@@ -2,3 +2,16 @@ import "./header.html";
 import "./header.scss";
 
 //alert("js ok");
+// onscroll effect : 
+window.onscroll = function () {
+    scrollIt();
+}
+
+function scrollIt() {
+    if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50 ){
+        let a = document.getElementById("mobile-menu");
+        a.style.bottom = "80rem";;
+    } else {
+        let a = document.getElementById("mobile-menu");
+    }
+}
