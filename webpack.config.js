91fbdfa8eee5a44c5438542493b73e05f853abcc
nzouda777 <rodriguenzouda@gmail.com>
2022-05-02 21:54:21 +0100

    UPDATE of theme

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ const devMode = process.env.NODE_ENV !== "production";
 
 const plugins = [];
 
-const CurrentThemeName = "hotlock";
+const CurrentThemeName = "1433";
 
 plugins.push(
   new MiniCssExtractPlugin({
