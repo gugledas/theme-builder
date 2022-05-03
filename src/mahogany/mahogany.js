ef4bd41dda633bd4ed2cb99451fcf24c7ef89d54
noumecha <noumechaivan@gmail.com>
2022-05-03 14:55:58 +0100

    maj : by tmc

--- src/mahogany/mahogany.js
+++ src/mahogany/mahogany.js
@@ -7,16 +7,16 @@ import "./lib/js/jquery-3.6.0.min";
 
 // importation des sections :
 import "./header/header"; // js to review ****
-//import "./hero-section/hero";
-//import "./consult-section/consult";
-//import "./type-section/type";
-//import "./offer-section/offer";
-//import "./projects-section/project";
-//import "./help-section/help"; //js to review ****
-//import "./testimonial-section/testimonial";
-//import "./flooring-section/flooring";
-//import "./guide-section/guide";
+import "./hero-section/hero";
+import "./consult-section/consult";
+import "./type-section/type";
+import "./offer-section/offer";
+import "./projects-section/project";
+import "./help-section/help"; //js to review ****
+import "./testimonial-section/testimonial";
+import "./flooring-section/flooring";
+import "./guide-section/guide";
 import "./free-home-section/free-home";
-//import "./update-section/update";
-//import "./discuss-section/discuss";
+import "./update-section/update";
+import "./discuss-section/discuss";
 import "./footer-section/footer";
