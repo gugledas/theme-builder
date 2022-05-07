4f1ed96348009d57e48a6973826bf4e7a3d0aa17
gugledas <azabzistany@gmail.com>
2022-05-07 14:15:35 +0100

    maj f_news

--- src/f_news/pages/home/home.js
+++ src/f_news/pages/home/home.js
@@ -2,7 +2,7 @@ import "../../sections/header/header";
 import "../../sections/bac-to-top/bac-top";
 // import "../../sections/hero-home/home-hero";
 
-// import "../../sections/featured-section/featured.js";
+//import "../../sections/featured-section/featured.js";
 // import "../../sections/tech-stories-section/tech";
 // import "../../sections/finance-section/finance";
 // import "../../sections/proposition/proposition";
@@ -21,14 +21,14 @@ import "../../sections/bac-to-top/bac-top";
 // import "../../sections/archive/archive";
 
 /* service */
-//import "../../sections/filter-section/filter-section";
+// import "../../sections/filter-section/filter-section";
 // import "../../sections/first-service/first-service.js";
-//import "../../sections/teaser-services/teaser-services";
+// import "../../sections/teaser-services/teaser-services";
 // import "../../sections/metro-service/metro-service";
 
 /* testimony */
 //import "../../sections/testimony/testimony";
 
 /* leadership */
-import "../../sections/leadership/leadership";
-import "../../sections/leader-mansory/leader-mansory";
+// import "../../sections/leadership/leadership";
+// import "../../sections/leader-mansory/leader-mansory";
