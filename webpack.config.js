717e70942a4e45247b4f2b3c8c4e9556b3726b99
nzouda777 <rodriguenzouda@gmail.com>
2022-05-09 09:48:35 +0100

    testimonials

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ const devMode = process.env.NODE_ENV !== "production";
 
 const plugins = [];
 
-const CurrentThemeName = "f_news";
+const CurrentThemeName = "1433";
 
 plugins.push(
   new MiniCssExtractPlugin({
