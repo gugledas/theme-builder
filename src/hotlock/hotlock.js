3861e75498e78c4e5fd638ff6b25cbd375c50992
nzouda777 <rodriguenzouda@gmail.com>
2022-05-09 15:47:17 +0100

    LINK add

--- src/hotlock/hotlock.js
+++ src/hotlock/hotlock.js
@@ -1,5 +1,9 @@
 import "@stephane888/wbu-atomique/scss/bootstrap-all.scss";
 import "@stephane888/wbu-atomique/js/bootstrap/all.js";
+import 'https://unpkg.com/swiper@8/swiper-bundle.min.js'
+import "https://code.jquery.com/jquery-3.3.1.slim.min.js"
+import "https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js"
+import "https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"
 import './header/header'
 //import './welcome-section/welcome'
 //import './security-info-section/security.js'
