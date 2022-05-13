a0211e9349a81dfa77d3f969c3fcead7dff14f50
noumecha <noumechaivan@gmail.com>
2022-05-13 08:45:33 +0100

    maj : by tmc

--- src/f_news/pages/home/home.js
+++ src/f_news/pages/home/home.js
@@ -1,69 +1,65 @@
-<<<<<<< HEAD
+
 import "../../sections/header/header";
 import "../../sections/bac-to-top/bac-top";
 import "../../sections/hero-home/home-hero";
-=======
->>>>>>> 84a31bf598556625341b76f7f95d3c727af143f7
-// import "../../sections/header/header";
-// import "../../sections/bac-to-top/bac-top";
-// import "../../sections/hero-home/home-hero";
 
-//  import "../../sections/featured-section/featured.js";
-//  import "../../sections/tech-stories-section/tech";
-//  import "../../sections/finance-section/finance";
-//  import "../../sections/proposition/proposition";
-//  import "../../sections/video-headlines/video";
-//  import "../../sections/in-case/in-case";
-//  import "../../sections/bottom-text/teaser";
+ import "../../sections/header/header";
+ import "../../sections/bac-to-top/bac-top";
+ import "../../sections/hero-home/home-hero";
 
-//  import "../../sections/footer/footer";
+  import "../../sections/featured-section/featured.js";
+  import "../../sections/tech-stories-section/tech";
+  import "../../sections/finance-section/finance";
+  import "../../sections/proposition/proposition";
+  import "../../sections/video-headlines/video";
+  import "../../sections/in-case/in-case";
+  import "../../sections/bottom-text/teaser";
+
+  import "../../sections/footer/footer";
 
 /* blog */
-//import "../../sections/hero-blog/hero-blog";
-// import "../../sections/sticky-author/sticky-author";
-// import "../../sections/blog-lists/blog";
-// import "../../sections/tag-list/tag-list";
+import "../../sections/hero-blog/hero-blog";
+ import "../../sections/sticky-author/sticky-author";
+ import "../../sections/blog-lists/blog";
+ import "../../sections/tag-list/tag-list";
 
 /* archive */
-// import "../../sections/archive/archive";
+ import "../../sections/archive/archive";
 
 /* service */
-<<<<<<< HEAD
+
  import "../../sections/filter-section/filter-section";
  import "../../sections/first-service/first-service.js";
  import "../../sections/teaser-services/teaser-services";
  import "../../sections/metro-service/metro-service";
 
-=======
->>>>>>> 84a31bf598556625341b76f7f95d3c727af143f7
-// import "../../sections/filter-section/filter-section";
-// import "../../sections/first-service/first-service.js";
-// import "../../sections/teaser-services/teaser-services";
-// import "../../sections/metro-service/metro-service";
-<<<<<<< HEAD
+ import "../../sections/filter-section/filter-section";
+ import "../../sections/first-service/first-service.js";
+ import "../../sections/teaser-services/teaser-services";
+ import "../../sections/metro-service/metro-service";
+
 import "../../sections/img-right-left/img-right-left";
 import "../../sections/text-btn-right/text-btn-right";
-=======
-// import "../../sections/img-right-left/img-right-left";
-// import "../../sections/text-btn-right/text-btn-right";
->>>>>>> 84a31bf598556625341b76f7f95d3c727af143f7
+
+ import "../../sections/img-right-left/img-right-left";
+ import "../../sections/text-btn-right/text-btn-right";
 
 /* testimony */
-// import "../../sections/testimony/testimony";
+ import "../../sections/testimony/testimony";
 
 /* leadership */
-// import "../../sections/leadership/leadership";
-// import "../../sections/leader-mansory/leader-mansory";
-//import "../../sections/testimony/testimony";
-//import "../../sections/testimony-2/testimony-2";
+ import "../../sections/leadership/leadership";
+ import "../../sections/leader-mansory/leader-mansory";
+import "../../sections/testimony/testimony";
+import "../../sections/testimony-2/testimony-2";
 
 /* leadership */
-// import "../../sections/leadership/leadership";
-// import "../../sections/leader-mansory/leader-mansory";
+ import "../../sections/leadership/leadership";
+ import "../../sections/leader-mansory/leader-mansory";
 
 /* contact */
-// import "../../sections/contact/contact";
-// import "../../sections/our-location/our-location";
+ import "../../sections/contact/contact";
+ import "../../sections/our-location/our-location";
 
 /* slideshow */
-// import "../../sections/slideshow-hero/slideshow-hero";
+ import "../../sections/slideshow-hero/slideshow-hero";
