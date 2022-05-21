d58631c6586249297e2d95d1f64b88f240cb4b3b
nzouda777 <rodriguenzouda@gmail.com>
2022-05-21 21:15:15 +0100

    maj

--- src/hotlock/hotlock.js
+++ src/hotlock/hotlock.js
@@ -2,14 +2,14 @@ import "@stephane888/wbu-atomique/scss/bootstrap-all.scss";
 import "@stephane888/wbu-atomique/js/bootstrap/all.js";
 
 import './header/header'
-//import './welcome-section/welcome'
-//import './security-info-section/security.js'
-//import './locksmith-services/lock.js'
+import './welcome-section/welcome'
+import './security-info-section/security.js'
+import './locksmith-services/lock.js'
 //import './team-section/team'
-//import './products-section/product'
+import './products-section/product'
 import './services-overview/overview'
-//import './footer/footer'
+import './footer/footer'
 
 // Teasers importations
 
-//import './teasers/teasers'
+import './teasers/teasers'
