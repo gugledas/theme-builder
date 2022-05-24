2547979e80916aaef8caefc9e2af351497bc6141
noumecha <noumechaivan@gmail.com>
2022-05-24 01:14:50 +0100

    maj: by tmc

--- src/f_news/pages/home/home.js
+++ src/f_news/pages/home/home.js
@@ -1,13 +1,13 @@
 // import "../../sections/header/header";
 // import "../../sections/bac-to-top/bac-top";
-// import "../../sections/hero-home/home-hero";
+import "../../sections/hero-home/home-hero";
 
 /* home */
-//import "../../sections/featured-section/featured.js";
+import "../../sections/featured-section/featured.js";
 // import "../../sections/tech-stories-section/tech";
 // import "../../sections/finance-section/finance";
 // import "../../sections/proposition/proposition";
-// import "../../sections/video-headlines/video";
+import "../../sections/video-headlines/video";
 // import "../../sections/in-case/in-case";
 // import "../../sections/bottom-text/teaser";
 
