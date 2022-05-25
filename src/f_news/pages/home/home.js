c2d123108dcbf710188a2ce86a13a7a305c4e9f7
gugledas <azabzistany@gmail.com>
2022-05-25 12:03:56 +0100

    maj f_news

--- src/f_news/pages/home/home.js
+++ src/f_news/pages/home/home.js
@@ -1,17 +1,17 @@
 // import "../../sections/header/header";
 // import "../../sections/bac-to-top/bac-top";
-import "../../sections/hero-home/home-hero";
+//import "../../sections/hero-home/home-hero";
 
 /* home */
-import "../../sections/featured-section/featured.js";
-import "../../sections/tech-stories-section/tech";
+// import "../../sections/featured-section/featured.js";
+// import "../../sections/tech-stories-section/tech";
 import "../../sections/finance-section/finance";
 // import "../../sections/proposition/proposition";
-// import "../../sections/video-headlines/video";
+import "../../sections/video-headlines/video";
 import "../../sections/in-case/in-case";
 // import "../../sections/bottom-text/teaser";
 
-import "../../sections/footer/footer";
+//import "../../sections/footer/footer";
 
 /* blog */
 // import "../../sections/hero-blog/hero-blog";
@@ -20,7 +20,7 @@ import "../../sections/footer/footer";
 // import "../../sections/tag-list/tag-list";
 
 /* archive */
-import "../../sections/archive/archive";
+//import "../../sections/archive/archive";
 
 /* service */
 
