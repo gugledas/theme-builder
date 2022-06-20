d3d7b1ef53aac661416ee26af2ef93cbe0ab39a8
noumecha <noumechaivan@gmail.com>
2022-06-20 21:29:29 +0100

    maj --tmc

--- src/hotlock/hotlock.js
+++ src/hotlock/hotlock.js
@@ -1,21 +1,6 @@
 import "@stephane888/wbu-atomique/scss/bootstrap-all.scss";
 import "@stephane888/wbu-atomique/js/bootstrap/all.js";
 
-<<<<<<< HEAD
-//import "./header/header";
-//import "./welcome-section/welcome";
-//import "./security-info-section/security.js";
-//import "./locksmith-services/lock.js";
-//import "./our-services-section/services";
-//import "./team-section/team";
-//import "./products-section/product";
-import "./services-overview/overview";
-//import "./footer/footer";
-//import "./testimony/testimony";
-// Teasers importations
-
-//import "./teasers/teasers";
-=======
 import "./header/header";
 import "./bac-to-top/bac-top";
 import "./welcome-section/welcome";
@@ -30,4 +15,3 @@ import "./testimony/testimony";
 import "./products-section/product";
 
 import "./footer/footer";
->>>>>>> 3d3c23f8daa4d5c5657f252ee1bf6bf9c1244177
