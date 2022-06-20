3d3c23f8daa4d5c5657f252ee1bf6bf9c1244177
gugledas <azabzistany@gmail.com>
2022-06-20 09:07:32 +0100

    maj hotlock

--- src/hotlock/hotlock.js
+++ src/hotlock/hotlock.js
@@ -4,14 +4,14 @@ import "@stephane888/wbu-atomique/js/bootstrap/all.js";
 import "./header/header";
 import "./bac-to-top/bac-top";
 import "./welcome-section/welcome";
-import "./security-info-section/security.js";
-import "./locksmith-services/lock.js";
 import "./our-services-section/services";
-import "./team-section/team";
-import "./products-section/product";
-import "./services-overview/overview";
-
-import "./testimony/testimony";
 // Teasers importations
 import "./teasers/teasers";
+import "./security-info-section/security.js";
+import "./locksmith-services/lock.js";
+import "./services-overview/overview";
+import "./team-section/team";
+import "./testimony/testimony";
+import "./products-section/product";
+
 import "./footer/footer";
