5f92ea07d1269fc5f262122048b9e4034486db5a
gugledas <azabzistany@gmail.com>
2022-07-18 07:50:29 +0100

    maj

--- src/f_news/pages/home/home.js
+++ src/f_news/pages/home/home.js
@@ -1,10 +1,10 @@
 import "../../sections/header/header";
 import "../../sections/bac-to-top/bac-top";
-//import "../../sections/hero-home/home-hero";
+import "../../sections/hero-home/home-hero";
 
 /* home */
 // import "../../sections/featured-section/featured.js";
-//import "../../sections/tech-stories-section/tech";
+// import "../../sections/tech-stories-section/tech";
 // import "../../sections/finance-section/finance";
 // import "../../sections/proposition/proposition";
 // import "../../sections/video-headlines/video";
@@ -14,7 +14,7 @@ import "../../sections/bac-to-top/bac-top";
 //import "../../sections/footer/footer";
 
 /* blog */
-//import "../../sections/hero-blog/hero-blog";
+// import "../../sections/hero-blog/hero-blog";
 // import "../../sections/sticky-author/sticky-author";
 //import "../../sections/blog-lists/blog";
 // import "../../sections/tag-list/tag-list";
