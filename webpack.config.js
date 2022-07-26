3a81788886d70af7bfccc46cfdd07f55c694895d
noumecha <noumechaivan@gmail.com>
2022-07-26 10:29:22 +0100

    maj : tmc

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ const devMode = process.env.NODE_ENV !== "production";
 const plugins = [];
 
 
-const CurrentThemeName = "sublime_r";
+const CurrentThemeName = "hongo";
 
 plugins.push(
   new MiniCssExtractPlugin({
