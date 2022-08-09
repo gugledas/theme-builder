2cddb8040cf523a4bacb15cc93a99b63317fd194
nzouda777 <rodriguenzouda@gmail.com>
2022-08-09 02:52:33 +0100

    maj

--- src/mitor/mitor.js
+++ src/mitor/mitor.js
@@ -2,6 +2,8 @@ import "./mitor.scss";
 import "@stephane888/wbu-atomique/js/bootstrap/all.js";
 
 // section
-import "./mitor-hero/top";
-import "./mitor-footer/footer";
+//import "./mitor-hero/top";
+//import "./mitor-footer/footer";
 //import "./menu-section/product";
+import "./mitor-services/service"
+import "./mitor-contact-us/contact"
