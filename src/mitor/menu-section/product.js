08744c7ead8e6343991411e9215f049f90d04fd9
gugledas <azabzistany@gmail.com>
2022-08-09 14:41:52 +0100

    maj

--- src/mitor/menu-section/product.js
+++
@@ -1,4 +0,0 @@
-import "./product.html";
-import "./product.scss";
-
-import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/instant-lunch/js/il-action-card.js";
--- src/mitor/mitor.js
+++ src/mitor/mitor.js
@@ -3,9 +3,6 @@ import "@stephane888/wbu-atomique/js/bootstrap/all.js";
 
 // section
 //import "./navbar/navbar";
-//import "./mitor-hero/top";
-//import "./mitor-services/service"
-//import "./menu-section/product";
-
-//import "./mitor-contact-us/contact"
+// import "./mitor-hero/top";
+// import "./mitor-services/service";
 // import "./mitor-footer/footer";
