b7450500b144373dc8f197ae4594606dcbc71439
gugledas <azabzistany@gmail.com>
2022-08-10 08:26:54 +0100

    maj

--- src/mitor/mitor.js
+++ src/mitor/mitor.js
@@ -3,8 +3,6 @@ import "@stephane888/wbu-atomique/js/bootstrap/all.js";
 
 // section
 //import "./mitor-hero/top";
-//import "./mitor-footer/footer";
-//import "./menu-section/product";
 //import "./mitor-services/service"
-//import "./mitor-contact-us/contact"
-import './mitor-testimony/testimony'
+//import "./mitor-testimony/testimony";
+//import "./mitor-footer/footer";
