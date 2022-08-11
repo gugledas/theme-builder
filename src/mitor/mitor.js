948bba58c68e3566d9f8d98a305d4806ba3db951
gugledas <azabzistany@gmail.com>
2022-08-11 13:53:44 +0100

    ajout mitor-card-blog

--- src/mitor/mitor.js
+++ src/mitor/mitor.js
@@ -3,9 +3,10 @@ import "@stephane888/wbu-atomique/js/bootstrap/all.js";
 
 //import "./navbar/navbar";
 //import "./mitor-hero/top";
-//import "./mitor-hero-2/top";
+import "./mitor-hero-2/top";
 //import "./mitor-services/service";
-//import "./mitor-card-img/top";
+import "./mitor-card-blog/service.js";
+import "./mitor-card-img/top";
 // import "./mitor-project-tabs/service";
 // import "./mitor-testimony/testimony";
 // import "./top-project-galery/service";
