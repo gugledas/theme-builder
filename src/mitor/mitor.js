464c7283264663210fd860174ac58d8498ddacbf
gugledas <azabzistany@gmail.com>
2022-08-11 09:10:12 +0100

    ajout mitor-overview

--- src/mitor/mitor.js
+++ src/mitor/mitor.js
@@ -1,14 +1,13 @@
 import "./mitor.scss";
 import "@stephane888/wbu-atomique/js/bootstrap/all.js";
 
-// section
-// section
-import "./navbar/navbar";
+//import "./navbar/navbar";
 //import "./mitor-hero/top";
-import "./mitor-hero-2/top";
-import "./mitor-services/service";
-import "./mitor-card-img/top";
+//import "./mitor-hero-2/top";
+//import "./mitor-services/service";
+//import "./mitor-card-img/top";
 // import "./mitor-testimony/testimony";
 //import "./top-project/service";
 //import "./mitor-footer/footer";
-import "./mitor-pricing/pricing";
+//import "./mitor-pricing/pricing";
+//import "./mitor-overview/top";
