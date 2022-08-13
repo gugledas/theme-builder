ce5bf9feecb50f8cd12d1656cab4a844b793e879
nzouda777 <rodriguenzouda@gmail.com>
2022-08-13 02:47:05 +0100

    maj

--- src/mitor/mitor.js
+++ src/mitor/mitor.js
@@ -17,5 +17,6 @@ import "@stephane888/wbu-atomique/js/bootstrap/all.js";
 //import "./mitor-testimony/testimony";
 // import "./top-project-galery/service";
 //import "./mitor-footer/footer";
-import "./mitor-pricing/pricing";
+//import "./mitor-pricing/pricing";
 //import "./mitor-overview/top";
+import './mitor-team/team'
