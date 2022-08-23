586d34959ce34345b8de0f73228e92e5d3a8c218
nzouda777 <rodriguenzouda@gmail.com>
2022-08-23 11:04:30 +0100

    countdown

--- src/mitor/mitor-hero/top.js
+++ src/mitor/mitor-hero/top.js
@@ -1,4 +1,4 @@
 import "./top.html";
 import "./top.scss";
-//import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/mitor/js/mitor-derived-header.js"
+import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/mitor/js/mitor-derived-header.js"
 
