e62031c2eca9fc453f7916f5289bf772996a58f8
nzouda777 <rodriguenzouda@gmail.com>
2022-08-25 09:39:23 +0100

    maj

--- src/mitor/mitor.js
+++ src/mitor/mitor.js
@@ -2,7 +2,7 @@ import "./mitor.scss";
 import "@stephane888/wbu-atomique/js/bootstrap/all.js";
 
 //import "./navbar/navbar";
-import "./mitor-hero/top";
+//import "./mitor-hero/top";
 //import "./mitor-hero-2/top";
 // import "./mitor-services/service";
 //import "./mitor-card-blog/service.js";
@@ -10,7 +10,7 @@ import "./mitor-hero/top";
 // import "./mitor-project-tabs/service";
 //import "./mitor-testimony/testimony";
 // import "./top-project-galery/service";
-//import "./mitor-footer/footer";
+import "./mitor-footer/footer";
 //import "./mitor-pricing/pricing";
 //import "./mitor-overview/top";
 //import './mitor-team/team'
@@ -24,7 +24,7 @@ import "./mitor-hero/top";
 //import "./mitor-contact-form/contac-right/contact"
 //import "./mitor-contact-form/contact-right/contact"
 //import './mitor-coming-soon/mitor-coming-soon'
-import "./mitor-coming-soon/mitor-coming-soon";
+//import "./mitor-coming-soon/mitor-coming-soon";
 //  import "./mitor-contact-hero/hero"
 // import "./mitor-why-our-service-teaser/why";
 // import "./question-pricing/question";
