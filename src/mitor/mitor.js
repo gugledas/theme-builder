97041ba7c806e6cd682f3f525b0cdcddfa2a6393
gugledas <azabzistany@gmail.com>
2022-08-25 16:01:57 +0100

    maj footer

--- src/mitor/mitor.js
+++ src/mitor/mitor.js
@@ -10,7 +10,7 @@ import "@stephane888/wbu-atomique/js/bootstrap/all.js";
 // import "./mitor-project-tabs/service";
 // import "./mitor-testimony/testimony";
 // import "./top-project-galery/service";
-//import "./mitor-footer/footer";
+import "./mitor-footer/footer";
 //import "./mitor-pricing/pricing";
 //import "./mitor-overview/top";
 //import './mitor-team/team'
