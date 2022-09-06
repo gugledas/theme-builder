e0342bce76c0b01d79447347eedb9352039bfc44
nzouda777 <rodriguenzouda@gmail.com>
2022-09-06 20:18:42 +0100

    countdown update

--- src/mitor/mitor.js
+++ src/mitor/mitor.js
@@ -4,15 +4,15 @@ import "@stephane888/wbu-atomique/js/bootstrap/all.js";
 //import "./navbar/navbar";
 //import "./mitor-hero/top";
 //import "./mitor-hero-2/top";
-import "./mitor-hero/top";
-import "./mitor-hero-2/top";
+//import "./mitor-hero/top";
+//import "./mitor-hero-2/top";
 // import "./mitor-services/service";
 //import "./mitor-card-blog/service.js";
 // import "./mitor-card-img/top";
 // import "./mitor-project-tabs/service";
 //import "./mitor-testimony/testimony";
 // import "./top-project-galery/service";
-import "./mitor-footer/footer";
+//import "./mitor-footer/footer";
 //import "./mitor-pricing/pricing";
 //import "./mitor-overview/top";
 //import './mitor-team/team'
@@ -26,7 +26,7 @@ import "./mitor-footer/footer";
 //import "./mitor-contact-form/contac-right/contact"
 //import "./mitor-contact-form/contact-right/contact"
 //import './mitor-coming-soon/mitor-coming-soon'
-//import "./mitor-coming-soon/mitor-coming-soon";
+import "./mitor-coming-soon/mitor-coming-soon";
 //  import "./mitor-contact-hero/hero"
 // import "./mitor-why-our-service-teaser/why";
 // import "./question-pricing/question";
