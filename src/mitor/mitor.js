7d4a09e96c7d28bbb3fe43d049cb4612fc4a90e7
nzouda777 <rodriguenzouda@gmail.com>
2022-09-23 16:01:32 +0100

    team teaser

--- src/mitor/mitor.js
+++ src/mitor/mitor.js
@@ -11,12 +11,12 @@ import "@stephane888/wbu-atomique/js/bootstrap/all.js";
 //import "./mitor-card-blog/service.js";
 // import "./mitor-card-img/top";
 // import "./mitor-project-tabs/service";
-import "./mitor-testimony/testimony";
+//import "./mitor-testimony/testimony";
 // import "./top-project-galery/service";
 //import "./mitor-footer/footer";
 //import "./mitor-pricing/pricing";
 //import "./mitor-overview/top";
-//import './mitor-team/team'
+import './mitor-team/team'
 //import "./mitor-blog/blogs"
 //import './mitor-404/404'
 //import "./mitor-contact-hero/hero"
@@ -27,7 +27,7 @@ import "./mitor-testimony/testimony";
 //import "./mitor-contact-form/contac-right/contact"
 //import "./mitor-contact-form/contact-right/contact"
 //import './mitor-coming-soon/mitor-coming-soon'
-import "./mitor-coming-soon/mitor-coming-soon";
+//import "./mitor-coming-soon/mitor-coming-soon";
 //  import "./mitor-contact-hero/hero"
 // import "./mitor-why-our-service-teaser/why";
 // import "./question-pricing/question";
--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ const devMode = process.env.NODE_ENV !== "production";
 
 const plugins = [];
 
-const CurrentThemeName = "Orphans-block";
+const CurrentThemeName = "mitor";
 
 plugins.push(
   new MiniCssExtractPlugin({
