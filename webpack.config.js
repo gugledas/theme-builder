934e89d4fde07399f395af84be21ac97ba8f23d8
noumecha spaker <noumechaivan@gmail.com>
2022-09-29 16:41:45 +0100

    maj : by tmc

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ const devMode = process.env.NODE_ENV !== "production";
 
 const plugins = [];
 
-const CurrentThemeName = "kave-home";
+const CurrentThemeName = "mitor"; // kave-home
 plugins.push(
   new MiniCssExtractPlugin({
     filename: "css/[name].css",
