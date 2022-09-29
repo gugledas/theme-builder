74cba725c3b23f3d6c3a41be4d28c18e58962973
nzouda777 <rodriguenzouda@gmail.com>
2022-09-29 15:39:49 +0100

    team teaser

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ const devMode = process.env.NODE_ENV !== "production";
 
 const plugins = [];
 
-const CurrentThemeName = "kave-home";
+const CurrentThemeName = "mitor";
 plugins.push(
   new MiniCssExtractPlugin({
     filename: "css/[name].css",
