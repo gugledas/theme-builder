5e3ee9569cb60325178df264864973f61d7c899b
nzouda777 <rodriguenzouda@gmail.com>
2022-10-03 14:35:48 +0100

    team teaser

--- src/mitor/mitor.js
+++ src/mitor/mitor.js
@@ -1,18 +1,18 @@
 import "./mitor.scss";
 import "@stephane888/wbu-atomique/js/bootstrap/all.js";
 
-//import "./mitor-core-value/core-value"
-//import "./navbar/navbar";
-//import "./mitor-hero/top";
+// import "./mitor-core-value/core-value"
+// import "./navbar/navbar";
+import "./mitor-hero/top";
 //import "./mitor-hero-2/top";
 // import "./mitor-services/service";
- import "./mitor-card-blog/service.js";
+// import "./mitor-card-blog/service.js";
 // import "./mitor-card-img/top";
 // import "./mitor-project-tabs/service";
 // import "./mitor-testimony/testimony";
 // import "./top-project-galery/service";
 //import "./mitor-footer/footer";
-//import "./mitor-pricing/pricing";
+import "./mitor-pricing/pricing";
 //import "./mitor-overview/top";
 //import './mitor-team/team'
 //import "./mitor-blog/blogs"
