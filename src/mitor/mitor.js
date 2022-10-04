916db7111702febce0d6c774bdedeab919347193
gugledas <azabzistany@gmail.com>
2022-10-04 16:19:00 +0100

    maj pricing

--- src/mitor/mitor.js
+++ src/mitor/mitor.js
@@ -1,8 +1,8 @@
 import "./mitor.scss";
-import "@stephane888/wbu-atomique/js/bootstrap/all.js";
+import "@stephane888/wbu-atomique/js/bootstrap/all.js"; 
 
-// import "./mitor-core-value/core-value"
-// import "./navbar/navbar";
+//import "./mitor-core-value/core-value"
+//import "./navbar/navbar";
 //import "./mitor-hero/top";
 //import "./mitor-hero-2/top";
 // import "./mitor-services/service";
@@ -12,15 +12,15 @@ import "@stephane888/wbu-atomique/js/bootstrap/all.js";
 // import "./mitor-testimony/testimony";
 // import "./top-project-galery/service";
 //import "./mitor-footer/footer";
-//import "./mitor-pricing/pricing";
+ import "./mitor-pricing/pricing";
 //import "./mitor-overview/top";
 //import './mitor-team/team'
 //import "./mitor-blog/blogs"
 //import './mitor-team/team'
-import "./mitor-blog/blogs"
+//import "./mitor-blog/blogs"
 //import './mitor-404/404'
 //import "./mitor-contact-hero/hero"
-//import './mitor-why-our-service-teaser/why'
+ //import './mitor-why-our-service-teaser/why'
 //import './question-pricing/question'
 //import './mitor-newsletter/newsletter'
 //import './mitor-card-image/card-img'
