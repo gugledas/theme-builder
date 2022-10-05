b01b605fa9d5ffdcb0d418aaadfb9aa6e07ede85
gugledas <azabzistany@gmail.com>
2022-10-05 11:36:48 +0100

    maj pricing et tab selection

--- src/mitor/mitor.js
+++ src/mitor/mitor.js
@@ -1,7 +1,7 @@
 import "./mitor.scss";
 import "@stephane888/wbu-atomique/js/bootstrap/all.js"; 
 
-import './mitor-aboutUs/about'
+ //import './mitor-aboutUs/about'
 //import './mitor-success-teaser/teaser'
  //import "./mitor-core-value/core-value"
 // import "./navbar/navbar";
@@ -11,20 +11,18 @@ import './mitor-aboutUs/about'
 //import "./mitor-hero-2/top";
 //import "./mitor-services/service";
 //import "./mitor-card-blog/service.js";
-// import "./mitor-card-img/top";
-// import "./mitor-project-tabs/service";
+//import "./mitor-card-img/top";
+//import "./mitor-project-tabs/service";
 // import "./mitor-testimony/testimony";
 // import "./top-project-galery/service";
 //import "./mitor-footer/footer";
- //import "./mitor-pricing/pricing";
+  import "./mitor-pricing/pricing";
 //import "./mitor-overview/top";
 //import './mitor-team/team'
 //import "./mitor-blog/blogs"
-//import './mitor-team/team'
-//import "./mitor-blog/blogs"
-//import './mitor-404/404'
+//import './mitor-team/team' 
 //import "./mitor-contact-hero/hero"
- //import './mitor-why-our-service-teaser/why'
+//import './mitor-why-our-service-teaser/why'
 //import './question-pricing/question'
 //import './mitor-newsletter/newsletter'
 //import './mitor-card-image/card-img'
