46997f5bbe089f3c60a0fe4903b50793eb49c291
noumecha spaker <noumechaivan@gmail.com>
2022-10-06 07:45:24 +0100

    maj : by tmc

--- src/mitor/mitor.js
+++ src/mitor/mitor.js
@@ -1,16 +1,6 @@
 import "./mitor.scss";
 import "@stephane888/wbu-atomique/js/bootstrap/all.js"; 
 
-<<<<<<< HEAD
-//import "./mitor-core-value/core-value"
-//import "./navbar/navbar";
-import "./mitor-hero/top";
-// import "./mitor-hero-2/top";
-// import "./mitor-services/service";
-// import "./mitor-card-blog/service.js";
-// import "./mitor-card-img/top";
- import "./mitor-project-tabs/service";
-=======
  //import './mitor-aboutUs/about'
 //import './mitor-success-teaser/teaser'
  //import "./mitor-core-value/core-value"
@@ -23,24 +13,13 @@ import "./mitor-hero/top";
 //import "./mitor-card-blog/service.js";
 //import "./mitor-card-img/top";
 //import "./mitor-project-tabs/service";
->>>>>>> b01b605fa9d5ffdcb0d418aaadfb9aa6e07ede85
 // import "./mitor-testimony/testimony";
  import "./top-project-galery/service";
 //import "./mitor-footer/footer";
-<<<<<<< HEAD
-import "./mitor-pricing/pricing";
-// import "./mitor-overview/top";
-//import './mitor-team/team'
-//import "./mitor-blog/blogs"
-//import './mitor-team/team'
-//import "./mitor-blog/blogs"
-//import './mitor-404/404'
-=======
   import "./mitor-pricing/pricing";
 //import "./mitor-overview/top";
 //import './mitor-team/team'
 //import "./mitor-blog/blogs"
 //import './mitor-team/team' 
->>>>>>> b01b605fa9d5ffdcb0d418aaadfb9aa6e07ede85
 //import "./mitor-contact-hero/hero"
 //import "./mitor-coming-soon/mitor-coming-soon";
