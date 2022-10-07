048dbf36618a6aafd86dae0951e28bea8cefc406
gugledas <azabzistany@gmail.com>
2022-10-07 12:12:33 +0100

    maj  pricing

--- src/mitor/mitor.js
+++ src/mitor/mitor.js
@@ -11,12 +11,10 @@ import "@stephane888/wbu-atomique/js/bootstrap/all.js";
 // import "./mitor-project-tabs/service";
 // import "./mitor-testimony/testimony";
 // import "./top-project-galery/service";
- import "./mitor-footer/footer";
- //import "./mitor-pricing/pricing";
- //import "./mitor-overview/top";
-//import './mitor-team/team'
-//import "./mitor-blog/blogs"
-//import './mitor-team/team'
+ //import "./mitor-footer/footer";
+// import "./mitor-pricing/pricing";
+ //import "./mitor-overview/top"; 
+ //import './mitor-team/team'
 //import "./mitor-blog/blogs"
 //import './mitor-404/404'
 //import "./mitor-contact-hero/hero"
