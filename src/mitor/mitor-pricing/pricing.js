af3747b29003307c0e2b0ba7f062a1be89b58773
gugledas <azabzistany@gmail.com>
2022-10-07 18:00:39 +0100

    maj pricing

--- src/mitor/mitor-pricing/pricing.js
+++ src/mitor/mitor-pricing/pricing.js
@@ -1,3 +1,4 @@
-import './pricing.html'
-import './pricing.scss'
-import '@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/mitor/js/mitor-pricing.js'
+import "./pricing.html";
+import "./pricing.scss";
+//import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/mitor/js/mitor-pricing.js";
+import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/mitor/js/mitor-check-selection.js";
