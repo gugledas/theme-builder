4cd0f9c7b474631c6fe064ada1ed5dba34d82d7a
gugledas <azabzistany@gmail.com>
2022-10-11 13:00:46 +0100

    maj project-tabs

--- src/mitor/mitor.js
+++ src/mitor/mitor.js
@@ -8,18 +8,18 @@ import "@stephane888/wbu-atomique/js/bootstrap/all.js";
 // import "./mitor-services/service";
 //import "./mitor-card-blog/service.js";
 // import "./mitor-card-img/top";
-// import "./mitor-project-tabs/service";
+import "./mitor-project-tabs/service";
 // import "./mitor-testimony/testimony";
 // import "./top-project-galery/service";
- //import "./mitor-footer/footer";
- //import "./mitor-pricing/pricing";
- //import "./mitor-overview/top"; 
- //import './mitor-team/team'
 //import "./mitor-footer/footer";
-import "./mitor-pricing/pricing";
+//import "./mitor-pricing/pricing";
+//import "./mitor-overview/top";
+//import './mitor-team/team'
+//import "./mitor-footer/footer";
+//import "./mitor-pricing/pricing";
 //import "./mitor-overview/top";
 //import './mitor-team/team'
 //import "./mitor-blog/blogs"
-import './mitor-404/404'
+//import './mitor-404/404'
 //import "./mitor-contact-hero/hero"
 //import "./mitor-coming-soon/mitor-coming-soon";
