f84fa93af06aa9f5a5105611db78e16982cdc873
nzouda777 <rodriguenzouda@gmail.com>
2022-10-12 00:43:13 +0100

    maj

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ const devMode = process.env.NODE_ENV !== "production";
 
 const plugins = [];
 
-const CurrentThemeName = "mitor";
+const CurrentThemeName = "Orphans-block";
 plugins.push(
   new MiniCssExtractPlugin({
     filename: "css/[name].css",
