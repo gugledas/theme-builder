31662fea28eed5409b706ec74d5f134feeb339a4
nzouda777 <rodriguenzouda@gmail.com>
2022-10-12 21:58:38 +0100

    maj footer agence constant

--- src/agenceconstant/agenceconstant.js
+++ src/agenceconstant/agenceconstant.js
@@ -3,5 +3,5 @@ import "@stephane888/wbu-atomique/js/bootstrap/all.js";
 
 // import des sections
 
-import "./ServicesSection/service";
+//import "./ServicesSection/service";
 import "./FooterSection/footer.js";
