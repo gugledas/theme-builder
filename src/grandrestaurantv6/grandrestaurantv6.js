2f9c48d80da9135405d83226b0b0ad0267362395
noumecha spaker <noumechaivan@gmail.com>
2022-10-24 21:14:10 +0100

    maj : by tmc hero section ok 80%

--- src/grandrestaurantv6/grandrestaurantv6.js
+++ src/grandrestaurantv6/grandrestaurantv6.js
@@ -3,7 +3,7 @@ import "@stephane888/wbu-atomique/js/bootstrap/all.js";
 
 // import des sections : 
 import "./hero/hero";
-import "./discover/discover";
-import "./menus-section/menus";
-import "./banner/banner";
-import "./footer/footer";
+//import "./discover/discover";
+//import "./menus-section/menus";
+//import "./banner/banner";
+//import "./footer/footer";
