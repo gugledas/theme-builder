c0ef207b3f0d8e5b0473d701b42a0e92e22a4298
noumecha spaker <noumechaivan@gmail.com>
2022-10-25 13:14:53 +0100

    maj : grandrestaurant hero section ok 98%

--- src/grandrestaurantv6/grandrestaurantv6.js
+++ src/grandrestaurantv6/grandrestaurantv6.js
@@ -2,8 +2,8 @@ import "./grandrestaurantv6.scss";
 import "@stephane888/wbu-atomique/js/bootstrap/all.js";
 
 // import des sections :
-import "./hero/hero";
-import "./discover/discover";
+//import "./hero/hero";
+//import "./discover/discover";
 //import "./menus-section/menus";
 //import "./banner/banner";
-//import "./footer/footer";
+import "./footer/footer";
