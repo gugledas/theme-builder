c1b880630a651c9f07be55912e112ca02ac8d69e
nzouda777 <rodriguenzouda@gmail.com>
2022-10-27 05:48:53 +0100

    grandrestaurant

--- src/grandrestaurantv6/grandrestaurantv6.js
+++ src/grandrestaurantv6/grandrestaurantv6.js
@@ -2,10 +2,10 @@ import "./grandrestaurantv6.scss";
 import "@stephane888/wbu-atomique/js/bootstrap/all.js";
 
 // import des sections :
-import "./hero/hero";
+//import "./hero/hero";
 //import "./discover/discover";
 //import "./menus-section/menus";
 //import "./banner/banner";
 //import "./footer/footer";
-//import "./cards/card-teaser/teaser";
+import "./cards/card-teaser/teaser";
 //import "./footer/footer";
