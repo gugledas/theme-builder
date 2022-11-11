9f05074a102f77fb0106f2e209aec2026e369f67
noumecha spaker <noumechaivan@gmail.com>
2022-11-11 08:50:34 +0100

    maj : by tmc

--- src/grandrestaurantv6/grandrestaurantv6.js
+++ src/grandrestaurantv6/grandrestaurantv6.js
@@ -8,11 +8,6 @@ import "./hero/hero";
 //import "./banner/banner";
 //import "./footer/footer";
 //import "./cards/card-teaser/teaser";
-<<<<<<< HEAD
-//import "./chief-card/chief-card";
-import "./grid-gallery/grid-gallery";
-=======
 import "./chief-card/chief-card";
 import "./section-card-text/section-card-text.js";
->>>>>>> 298451f9a5b56514345e8e9b23ceb00f06d26798
 //import "./footer/footer";
