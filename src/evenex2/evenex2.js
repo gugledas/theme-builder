8acc3b7a573e55e55df3a19b33b0fec40aafba68
kouwa stephane <kksasteph888@gmail.com>
2022-12-03 10:08:40 +0100

    Maj

--- src/evenex2/evenex2.js
+++ src/evenex2/evenex2.js
@@ -1,9 +1,9 @@
 import "./evenex.scss";
 import "@stephane888/wbu-atomique/js/bootstrap/all.js";
 
-//import "./evenex-countdown/countdown"
+import "./evenex-countdown/countdown";
 //import "./evenex-teasers/evenex-teaser"
 //import "./evenex-become-sponsor/sponsor"
 //import "./evenex-hurry-up/hurry-up"
 //import "./evenex-pricing/pricing"
-import "./evenex-newsroom/newsroom"
+import "./evenex-newsroom/newsroom";
