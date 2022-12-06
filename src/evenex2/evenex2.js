5ac681d783f399d78bcbc9b6e54ebb290ecaeca8
justin <justinonomo@gmail.com>
2022-12-06 10:38:08 +0100

    add evenex footer

--- src/evenex2/evenex2.js
+++ src/evenex2/evenex2.js
@@ -3,9 +3,10 @@ import "@stephane888/wbu-atomique/js/bootstrap/all.js";
 
 //import "./evenex-countdown/countdown"
 //import "./evenex-teasers/evenex-teaser"
-import "./evenex-become-sponsor/sponsor"
+//import "./evenex-become-sponsor/sponsor"
 //import "./evenex-hurry-up/hurry-up"
 //import "./evenex-pricing/pricing"
 //import "./evenex-newsroom/newsroom"
 //import "./evenex-last-year/last-year"
 //import "./evenex-memories/memories"
+import "./evenex-footer/footer"
