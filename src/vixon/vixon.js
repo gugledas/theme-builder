44ff3330231b24dfbaedab059a96acc85e9565f9
gugledas <azabzistany@gmail.com>
2022-12-13 15:04:26 +0100

    ajout vixon plane-banner-slide

--- src/vixon/vixon.js
+++ src/vixon/vixon.js
@@ -2,13 +2,14 @@ import "@stephane888/wbu-atomique/js/bootstrap/all.js";
 import "./vixon.scss";
 
  import "./entete/header";
- import "./vixcon-footer-section/footer";
- import "./sponsor/sponsor";
-
- import "./vixon-section-title/section-title";
- import "./faq-section/faq";
- import "./event-section/event";
  import "./counter-section/counter";
- import "./about-section/about";
-import "./location-section/location";
-import "./section-schedule/schedule";
+ import "./plane-banner-slide/counter";
+ import "./event-section/event";
+//  import "./vixcon-footer-section/footer";
+ ///import "./sponsor/sponsor";
+
+ //import "./vixon-section-title/section-title";
+ //import "./faq-section/faq";
+//  import "./about-section/about";
+// import "./location-section/location";
+// import "./section-schedule/schedule";
