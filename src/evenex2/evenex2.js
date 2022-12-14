3cc5c3dfdf05cb8098fca0a08774e565a688dca7
gugledas <azabzistany@gmail.com>
2022-12-14 11:35:38 +0100

    ajout section shedule event

--- src/evenex2/evenex2.js
+++ src/evenex2/evenex2.js
@@ -3,7 +3,7 @@ import "@stephane888/wbu-atomique/js/bootstrap/all.js";
 
 //import "./evenex-countdown/countdown"
 //import "./evenex-teasers/evenex-teaser"
-//import "./evenex-become-sponsor/sponsor"
+ import "./evenex-become-sponsor/sponsor"
 //import "./evenex-hurry-up/hurry-up"
 //import "./evenex-pricing/pricing"
 //import "./evenex-newsroom/newsroom"
@@ -11,7 +11,8 @@ import "@stephane888/wbu-atomique/js/bootstrap/all.js";
 //import "./evenex-memories/memories"
 //import "./evenex-footer/footer"
 //import "./evenex-speaker/speaker"
-//import "./evenex-schedule/schedule"
+ import "./evenex-schedule/schedule"
+ import "./evenex-event-shedule/shedule"
 //import "./evenex-stat-banner/stat"
 //import "./evenex-sponsor/sponsor"
 //import "./evenex-why-attend-event/attend"
