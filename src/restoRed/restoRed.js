a3e1325a7fcbfea1bce90d3b14d14e9d6bafcfba
Landry Gaby <ligalandry@gmail.com>
2023-03-10 17:05:03 +0100

    creation des teaser et de la section menuswiper

--- src/restoRed/restoRed.js
+++ src/restoRed/restoRed.js
@@ -2,3 +2,4 @@ import "./restoRed.scss";
 import "./redModel/redModel";
 import "./sectionService/sectionService"
 import "./sectionReservation/sectionReservation"
+import "./sectionMenuSwiper/sectionMenuSwiper"
