d33d7104e3415b283c0f20dcdd206eab9b5acf36
Landry Gaby <ligalandry@gmail.com>
2023-03-15 17:16:11 +0100

    ajout des screenshot

--- src/grandrestaurantv6/grandrestaurantv6.js
+++ src/grandrestaurantv6/grandrestaurantv6.js
@@ -3,12 +3,12 @@ import "@stephane888/wbu-atomique/js/bootstrap/all.js";
 
 // import des sections :
 import "./hero/hero";
-//import "./discover/discover";
-//import "./menus-section/menus";
-//import "./banner/banner";
-//import "./footer/footer";
+import "./discover/discover";
+import "./menus-section/menus";
+import "./banner/banner";
+import "./footer/footer";
 import "./grid-gallery/grid-gallery";
 import "./cards/card-teaser/teaser";
-//import "./chief-card/chief-card";
-//import "./counter-section/counter-section";
-//import "./footer/footer";
+import "./chief-card/chief-card";
+import "./counter-section/counter-section";
+import "./footer/footer";
--- src/kave-home/kave-home.js
+++ src/kave-home/kave-home.js
@@ -1,9 +1,9 @@
 /* theme style */
 import "./kave-home.scss";
 
-//import "@stephane888/wbu-atomique/js/bootstrap/all.js";
+import "@stephane888/wbu-atomique/js/bootstrap/all.js";
 // html
 import "./advantage-bloc/block";
 import "./mosaic-img/mosaic";
-// import "./type-parquet-section/parquet";
-// import "./article-teaser/article";
+ import "./type-parquet-section/parquet";
+ import "./article-teaser/article";
