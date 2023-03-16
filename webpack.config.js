1a5d3576ecf467a3027291c83019e703673132f7
Landry Gaby <ligalandry@gmail.com>
2023-03-16 12:26:05 +0100

    ajout des champs

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ const plugins = [];
 /**
  * Selectionner le theme à afficher lors de la commande npm run serve.
  */
-const CurrentThemeName = "plumbing";
+const CurrentThemeName = "firstmodel";
 plugins.push(
   new MiniCssExtractPlugin({
     filename: "css/[name].css",
