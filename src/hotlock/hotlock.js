fa73fd3e1f3d4e37035058f4dad6b7f35c190f4a
kouwa stephane <kksasteph888@gmail.com>
2023-03-16 11:47:37 +0100

    Maj

--- src/hotlock/hotlock.js
+++ src/hotlock/hotlock.js
@@ -2,16 +2,16 @@ import "./hotlock.scss";
 import "@stephane888/wbu-atomique/js/bootstrap/all.js";
 
 import "./header/header";
-import "./bac-to-top/bac-top";
-import "./welcome-section/welcome";
-import "./our-services-section/services";
-// Teasers importations
-import "./teasers/teasers";
-import "./security-info-section/security.js";
-import "./locksmith-services/lock.js";
-import "./services-overview/overview";
-import "./team-section/team";
-import "./testimony/testimony";
-import "./products-section/product";
+// import "./bac-to-top/bac-top";
+// import "./welcome-section/welcome";
+// import "./our-services-section/services";
+// // Teasers importations
+// import "./teasers/teasers";
+// import "./security-info-section/security.js";
+// import "./locksmith-services/lock.js";
+// import "./services-overview/overview";
+// import "./team-section/team";
+// import "./testimony/testimony";
+// import "./products-section/product";
 
-import "./footer/footer";
+// import "./footer/footer";
