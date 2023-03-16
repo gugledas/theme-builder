6cec9a60a92e6bc6747ff1dc7ed87d7700d4dac9
Landry Gaby <ligalandry@gmail.com>
2023-03-16 11:21:34 +0100

    ajout des screenshot

--- src/mitor/mitor.js
+++ src/mitor/mitor.js
@@ -1,27 +1,27 @@
 import "./mitor.scss";
 import "@stephane888/wbu-atomique/js/bootstrap/all.js";
 
- //import "./mitor-faq/faq"
-//import "./mitor-core-value/core-value"
-//import "./navbar/navbar";
-//import "./mitor-hero/top";
-// import "./mitor-hero-2/top";
-// import "./mitor-services/service";
-//import "./mitor-card-blog/service.js";
-// import "./mitor-card-img/top";
-//import "./mitor-card-image/card-img";
-//import "./mitor-project-tabs/service";
-// import "./mitor-testimony/testimony";
-// import "./top-project-galery/service";
-//import "./mitor-footer/footer";
-//import "./mitor-pricing/pricing";
-//import "./mitor-overview/top";
-//import './mitor-team/team'
-//import "./mitor-footer/footer";
-//import "./mitor-pricing/pricing";
-//import "./mitor-overview/top";
-//import './mitor-team/team'
-import "./mitor-blog/blogs"
-//import './mitor-404/404'
-//import "./mitor-contact-hero/hero"
-//import "./mitor-coming-soon/mitor-coming-soon";
+ import "./mitor-faq/faq"
+import "./mitor-core-value/core-value"
+import "./navbar/navbar";
+import "./mitor-hero/top";
+ import "./mitor-hero-2/top";
+ import "./mitor-services/service";
+import "./mitor-card-blog/service.js";
+ import "./mitor-card-img/top";
+import "./mitor-card-image/card-img";
+import "./mitor-project-tabs/service";
+ import "./mitor-testimony/testimony";
+ import "./top-project-galery/service";
+import "./mitor-footer/footer";
+import "./mitor-pricing/pricing";
+import "./mitor-overview/top";
+import './mitor-team/team'
+import "./mitor-footer/footer";
+import "./mitor-pricing/pricing";
+import "./mitor-overview/top";
+import './mitor-team/team'
+//import "./mitor-blog/blogs"
+import './mitor-404/404'
+import "./mitor-contact-hero/hero"
+import "./mitor-coming-soon/mitor-coming-soon";
--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ const plugins = [];
 /**
  * Selectionner le theme à afficher lors de la commande npm run serve.
  */
-const CurrentThemeName = "market-place";
+const CurrentThemeName = "plumbing";
 plugins.push(
   new MiniCssExtractPlugin({
     filename: "css/[name].css",
