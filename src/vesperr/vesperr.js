49c3d51108616b2322de87d2932532332bab93ae
Landry Gaby <ligalandry@gmail.com>
2023-03-17 12:53:44 +0100

    ajoust des screenshot

--- src/vesperr/vesperr.js
+++ src/vesperr/vesperr.js
@@ -9,19 +9,19 @@ import AOS from "./lib/aos/aos";
 
 // == importations des section du theme === //
 
-//import "./headers/header.js";
-//import "./HeroSection/hero.js";
-//import "./ClientSection/client.js";
-//import "./AboutSection/about.js";
-//import "./CountsSection/count.js";
-//import "./ServicesSection/services.js";
-//import "./MoreServicesSection/more.js";
-//import "./FeatureSection/feature.js";
-//import "./TestimonialSection/testimonial.js";
-//import "./PortofolioSection/portofolio.js";
-//import "./TeamSection/team.js";
-//import "./PriceSection/price.js";
-//import "./FaqSection/faq.js";
+import "./headers/header.js";
+import "./HeroSection/hero.js";
+import "./ClientSection/client.js";
+import "./AboutSection/about.js";
+import "./CountsSection/count.js";
+import "./ServicesSection/services.js";
+import "./MoreServicesSection/more.js";
+import "./FeatureSection/feature.js";
+import "./TestimonialSection/testimonial.js";
+import "./PortofolioSection/portofolio.js";
+import "./TeamSection/team.js";
+import "./PriceSection/price.js";
+import "./FaqSection/faq.js";
 import "./ContactSection/contact.js";
 //import "./FooterSection/footer.js";
 //import "./bac-to-top/bac-top";
