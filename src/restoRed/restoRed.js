f86e98d96eae1cdf08a9eedff3706a3d340baa0f
Landry Gaby <ligalandry@gmail.com>
2023-03-24 18:52:25 +0100

    creation de sectionCountDown

--- src/restoRed/restoRed.js
+++ src/restoRed/restoRed.js
@@ -1,9 +1,10 @@
 import "./restoRed.scss";
 import "./redModel/redModel";
- import "./sectionService/sectionService"
- import "./sectionReservation/sectionReservation"
+  import "./sectionService/sectionService"
+  import "./sectionReservation/sectionReservation"
  import "./sectionMenuSwiper/sectionMenuSwiper"
- import "./sectionPopular/sectionPopular"
- import "./sectionTeam/sectionTeam"
- import "./sectionBlogs/sectionBlogs"
- import "./restoRedFooter/restoRedFooter"
+  import "./sectionPopular/sectionPopular"
+  import "./sectionTeam/sectionTeam"
+  import "./sectionCountDown/sectionCountDown"
+  import "./sectionBlogs/sectionBlogs"
+  import "./restoRedFooter/restoRedFooter"
