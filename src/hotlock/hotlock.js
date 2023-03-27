1f287089c73c72004d2457e0d8ab68b443701d37
kouwa stephane <kksasteph888@gmail.com>
2023-03-27 11:10:55 +0200

    Maj cv buta pest

--- src/hotlock/hotlock.js
+++ src/hotlock/hotlock.js
@@ -1,17 +1,19 @@
 import "./hotlock.scss";
 import "@stephane888/wbu-atomique/js/bootstrap/all.js";
 
-import "./header/header";
+// import "./header/header";
 // import "./bac-to-top/bac-top";
-// import "./welcome-section/welcome";
-// import "./our-services-section/services";
-// // Teasers importations
-// import "./teasers/teasers";
-// import "./security-info-section/security.js";
-// import "./locksmith-services/lock.js";
-// import "./services-overview/overview";
-// import "./team-section/team";
-// import "./testimony/testimony";
-// import "./products-section/product";
+import "./welcome-section/welcome";
+import "./our-services-section/services";
+// Teasers importations
+import "./teasers/teasers";
+import "./security-info-section/security.js";
+import "./locksmith-services/lock.js";
+import "./services-overview/overview";
+import "./team-section/team";
+import "./testimony/testimony";
+import "./products-section/product";
 
 // import "./footer/footer";
+// buttons
+// import "./buttons/index";
--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ const plugins = [];
 /**
  * Selectionner le theme à afficher lors de la commande npm run serve.
  */
-const CurrentThemeName = "firstmodel";
+const CurrentThemeName = "cvModelBudapest";
 plugins.push(
   new MiniCssExtractPlugin({
     filename: "css/[name].css",
