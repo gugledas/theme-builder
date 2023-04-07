db1071d467a6b71065431dc02b2da6faad1e037d
kouwa stephane <kksasteph888@gmail.com>
2023-04-07 11:11:24 +0200

    Maj

--- src/facMaster/facMaster.js
+++ src/facMaster/facMaster.js
@@ -1,2 +1,35 @@
+import "@stephane888/wbu-atomique/js/bootstrap/all.js";
 import "./facMaster.scss";
-import "./facmaster-hero/facmaster-hero";
+import "./Heder/header";
+//import "./facmaster-hero/facmaster-hero";
+/**
+ * Permet d'injecter les librairies js.
+ * @param {*} src
+ * @returns
+ */
+function loadScript(src) {
+  return new Promise((resolv) => {
+    const s = document.createElement("script");
+    s.setAttribute("src", src);
+    s.onload = function () {
+      console.log("Chargement du script ok : ", src);
+      resolv(true);
+    };
+    document.head.appendChild(s);
+  });
+}
+function loadstyle(src) {
+  return new Promise((resolv) => {
+    var s = document.createElement("link");
+    s.setAttribute("rel", "stylesheet");
+    s.setAttribute("href", src);
+    s.onload = function () {
+      console.log("Chargement du Style ok : ", src);
+      resolv(true);
+    };
+    document.head.appendChild(s);
+  });
+}
+loadstyle(
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.1/css/all.min.css"
+);
