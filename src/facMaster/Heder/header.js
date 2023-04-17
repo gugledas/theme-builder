b60619389dd993e21ac56c43d2c2273aa0ba4671
Landry Gaby <ligalandry@gmail.com>
2023-04-17 18:01:24 +0100

    maj

--- src/facMaster/Heder/header.js
+++ src/facMaster/Heder/header.js
@@ -2,3 +2,4 @@ import "./header.html";
 import "@stephane888/wbu-atomique/js/scroll-detect/index.js";
 import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/f_news/js/first-nav.js";
 import "./header.scss";
+import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/facmaster/js/menu.js";
